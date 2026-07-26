ff4781a2dff5244b2562661563c645e237a5c88f
Khairi <khairi.240180150@mhs.unimal.ac.id>
2026-07-27 02:03:33 +0700

    Remove redundant DictionarySeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CountrySeeder::class,
             SentimentWordsSeeder::class,
-            DictionarySeeder::class,
             PortsSeeder::class,
             DemoUserSeeder::class,
         ]);
